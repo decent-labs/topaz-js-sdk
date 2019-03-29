de56e8959e7c8bbfb01df502d26bf36d0aced0a3
Adam Gall <adam@decentcrypto.com>
2019-03-29 13:43:32 -0400

    Add "random" helper to tests

---
+++ test/helpers/random.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto');
+
+hexString = (length) => {
+  let rs = crypto.randomBytes((length + 1) / 2).toString('hex');
+  if (length % 2 == 1) rs = rs.substring(0, rs.length - 1);
+  return rs;
+}
+
+sha256HexHash = () => {
+  const hash = crypto.createHmac('sha256', hexString(10)).update(hexString(10)).digest('hex');
+  return hash;
+}
+
+module.exports = { hexString, sha256HexHash };
