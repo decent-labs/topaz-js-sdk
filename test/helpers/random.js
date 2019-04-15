428d5131adf2dbd73a2fffb178d0a604f0d01e90
Adam Gall <adam@decentcrypto.com>
2019-04-15 10:56:00 -0400

    Add "const" modifiers to exported functions in random.js

--- test/helpers/random.js
+++ test/helpers/random.js
@@ -1,12 +1,12 @@
 const crypto = require('crypto');
 
-hexString = (length) => {
+const hexString = (length) => {
   let rs = crypto.randomBytes((length + 1) / 2).toString('hex');
   if (length % 2 == 1) rs = rs.substring(0, rs.length - 1);
   return rs;
 }
 
-sha256HexHash = () => {
+const sha256HexHash = () => {
   const hash = crypto.createHmac('sha256', hexString(10)).update(hexString(10)).digest('hex');
   return hash;
 }
