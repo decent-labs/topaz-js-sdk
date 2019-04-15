94050d70bef93990a15359980361341c72428fc0
Adam Gall <adam@decentcrypto.com>
2019-04-15 10:57:52 -0400

    Create a new helper function in random.js which outputs a random Base58 encoded sha256 multihash formatted hash

--- test/helpers/random.js
+++ test/helpers/random.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const multihashes = require('multihashes')
 
 const hexString = (length) => {
   let rs = crypto.randomBytes((length + 1) / 2).toString('hex');
@@ -11,4 +12,13 @@ const sha256HexHash = () => {
   return hash;
 }
 
-module.exports = { hexString, sha256HexHash };
+const sha256Base58Multihash = () => {
+  const secret = hexString(6);
+  const rando = crypto.randomBytes(1024);
+  const hash = crypto.createHmac('sha256', secret).update(rando).digest();
+  const multi = multihashes.encode(hash, 'sha2-256')
+  const b58 = multihashes.toB58String(multi);
+  return b58;
+}
+
+module.exports = { hexString, sha256HexHash, sha256Base58Multihash };
