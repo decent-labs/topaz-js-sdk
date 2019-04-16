465628c775204e5a962e9701912ffb25fd012644
Adam Gall <adam@decentcrypto.com>
2019-04-16 16:37:44 -0400

    Import and use the new hashing util when generating random base58 hash

--- test/helpers/random.js
+++ test/helpers/random.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const multihashes = require('multihashes')
+const multihash = require('../../src/utils/hashing')
 
 const hexString = (length) => {
   let rs = crypto.randomBytes((length + 1) / 2).toString('hex');
@@ -13,12 +13,9 @@ const sha256HexHash = () => {
 }
 
 const sha256Base58Multihash = () => {
-  const secret = hexString(6);
   const rando = crypto.randomBytes(1024);
-  const hash = crypto.createHmac('sha256', secret).update(rando).digest();
-  const multi = multihashes.encode(hash, 'sha2-256')
-  const b58 = multihashes.toB58String(multi);
-  return b58;
+  const hash = multihash(rando);
+  return hash;
 }
 
 module.exports = { hexString, sha256HexHash, sha256Base58Multihash };
