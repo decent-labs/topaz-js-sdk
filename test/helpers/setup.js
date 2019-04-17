dad2a6284666847db69938c6a03cff8a8233e6a3
Adam Gall <adam@decentcrypto.com>
2019-04-17 15:48:33 -0400

    Update test setup helper to create a new app on fresh instance

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -5,30 +5,29 @@ const TopazApi = require('../../src/topaz');
 const topaz = require('../../src/index');
 const random = require('./random');
 
-const apiKey = () => {
+const apiSetup = () => {
   const email = random.hexString(6) + '@' + random.hexString(6) + '.com';
   const password = random.hexString(20);
 
   return new Promise((resolve, reject) => {
     axios.post(process.env.BASE_PATH + '/users', { email, password })
       .then(_ => axios.post(process.env.BASE_PATH + '/auth/login', { email, password }))
-      .then(res => axios.post(process.env.BASE_PATH + '/tokens', {}, { headers: { 'Authorization': res.data.token } }))
-      .then(res => res.data.token)
-      .then(apiKey => resolve(apiKey))
+      .then(res => axios.post(process.env.BASE_PATH + '/tokens', {}, { headers: { 'Authorization': res.data.token }}))
+      .then(res => Promise.all([res.data.token, axios.post(process.env.BASE_PATH + '/apps', { name: 'test app', interval: 3600 }, { headers: { 'Authorization': res.data.token }})]))
+      .then(([ apiKey, res ]) => resolve({ apiKey, appId: res.data.id }))
       .catch(err => reject(err));
   });
 }
 
 const freshInstanceLegacy = () => {
-  return apiKey().then(apiKey => {
-    const opts = { basePath: process.env.BASE_PATH, apiKey };
-    return new TopazApi.ApiClient(opts)
+  return apiSetup().then(({ apiKey, appId }) => {
+    return new TopazApi.ApiClient({ basePath: process.env.BASE_PATH, apiKey, appId })
   });
 }
 
 const freshInstance = () => {
-  return apiKey().then(apiKey => {
-    return topaz({ basePath: process.env.BASE_PATH, apiKey });
+  return apiSetup().then(({ apiKey, appId }) => {
+    return topaz({ basePath: process.env.BASE_PATH, apiKey, appId });
   });
 }
 
