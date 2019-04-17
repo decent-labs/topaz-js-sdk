799cdf7e67041d4fee926dca34e1ccb0c23d8e7c
Adam Gall <adam@decentcrypto.com>
2019-04-17 13:09:08 -0400

    Update the api tests to include both legacy and new api interfaces

--- test/api/AppsApi.spec.js
+++ test/api/AppsApi.spec.js
@@ -2,87 +2,114 @@ const expect = require('expect.js');
 const setup = require('../helpers/setup');
 const TopazApi = require('../../src/topaz');
 
-describe('AppsApi', function() {
-  let appsApi;
-
-  beforeEach('get a fresh api instance', function(done) {
-    setup.freshInstanceLegacy().then(api => {
-      appsApi = new TopazApi.AppsApi(api);
-      done();
-    });
-  });
-
-  describe('createApp', function() {
-    const params = { name: 'my first app', interval: 3600 };
-
-    const expects = (params, data, response, done) => {
-      expect(data.name).to.be(params.name);
-      expect(data.interval).to.be(params.interval);
-      expect(response.statusCode).to.be(201);
-      done();
-    };
-
-    it('should call createApp successfully using promises', function(done) {  
-      appsApi.createApp(params).then(({ data, response }) => {
-        expects(params, data, response, done);
+describe('AppsApi', () => {
+  const appsTests = (create, find, get) => {
+    describe('createApp', () => {
+      const params = { name: 'my first app', interval: 3600 };
+  
+      const expects = (params, data, response, done) => {
+        expect(data.name).to.be(params.name);
+        expect(data.interval).to.be(params.interval);
+        expect(response.statusCode).to.be(201);
+        done();
+      };
+  
+      it('should call createApp successfully using promises', (done) => {
+        create()(params).then(({ data, response }) => {
+          expects(params, data, response, done);
+        });
+      });
+  
+      it('should call createApp successfully using callbacks', (done) => {
+        create()(params, (_, data, response) => {
+          expects(params, data, response, done);
+        });
       });
     });
-
-    it('should call createApp successfully using callbacks', function(done) {
-      appsApi.createApp(params, (_, data, response) => {
-        expects(params, data, response, done);
+  
+    describe('findApps', () => {
+      const expects = (data, response, done) => {
+        expect(data).to.have.length(2);
+        expect(response.statusCode).to.be(200);
+        done();
+      };
+  
+      it('should call findApps successfully using promises', (done) => {
+        create()({ name: 'a', interval: 30 })
+        .then(() => create()({ name: 'b', interval: 30 }))
+        .then(() => find()())
+        .then(({ data, response }) => {
+          expects(data, response, done)
+        });
       });
-    });
-  });
-
-  describe('findApps', function() {
-    const expects = (data, response, done) => {
-      expect(data).to.have.length(2);
-      expect(response.statusCode).to.be(200);
-      done();
-    };
-
-    it('should call findApps successfully using promises', function(done) {
-      appsApi.createApp({ name: 'a', interval: 30 })
-      .then(() => appsApi.createApp({ name: 'b', interval: 30 }))
-      .then(() => appsApi.findApps())
-      .then(({ data, response }) => {
-        expects(data, response, done)
-      });
-    });
-
-    it('should call findApps successfully using callbacks', function(done) {
-      appsApi.createApp({ name: 'a', interval: 30 }, () => {
-        appsApi.createApp({ name: 'b', interval: 30 }, () => {
-          appsApi.findApps((_, data, response) => {
-            expects(data, response, done)
+  
+      it('should call findApps successfully using callbacks', (done) => {
+        create()({ name: 'a', interval: 30 }, () => {
+          create()({ name: 'b', interval: 30 }, () => {
+            find()((_, data, response) => {
+              expects(data, response, done)
+            });
           });
         });
       });
     });
-  });
-
-  describe('getApp', function() {
-    const expects = (appId, data, response, done) => {
-      expect(data.id).to.be(appId);
-      expect(response.statusCode).to.be(200);
-      done();
-    };
-
-    it('should call getApp successfully using promises', function(done) {
-      appsApi.createApp({ name: 'other app', interval: 3600 })
-      .then(({ data, _ }) => Promise.all([appsApi.getApp(data.id), data.id]))
-      .then(([{ data, response }, appId]) => {
-        expects(appId, data, response, done)
+  
+    describe('getApp', () => {
+      const expects = (appId, data, response, done) => {
+        expect(data.id).to.be(appId);
+        expect(response.statusCode).to.be(200);
+        done();
+      };
+  
+      it('should call getApp successfully using promises', (done) => {
+        create()({ name: 'other app', interval: 3600 })
+        .then(({ data, _ }) => Promise.all([get()(data.id), data.id]))
+        .then(([{ data, response }, appId]) => {
+          expects(appId, data, response, done)
+        });
       });
-    });
-
-    it('should call getApp successfully using callbacks', function(done) {
-      appsApi.createApp({ name: 'other app', interval: 3600 }, (_, createData, __) => {
-        appsApi.getApp(createData.id, (_, getData, getResponse) => {
-          expects(createData.id, getData, getResponse, done);
+  
+      it('should call getApp successfully using callbacks', (done) => {
+        create()({ name: 'other app', interval: 3600 }, (_, createData, __) => {
+          get()(createData.id, (_, getData, getResponse) => {
+            expects(createData.id, getData, getResponse, done);
+          });
         });
       });
     });
+  };
+
+  describe('legacy interface', () => {
+    let appsApi;
+
+    beforeEach('get a fresh api instance', (done) => {
+      setup.freshInstanceLegacy().then(api => {
+        appsApi = new TopazApi.AppsApi(api);
+        done();
+      });
+    });
+
+    appsTests(
+      () => appsApi.createApp,
+      () => appsApi.findApps,
+      () => appsApi.getApp
+    );
+  });
+
+  describe('new interface', () => {
+    let apps;
+
+    beforeEach('get a fresh api instance', (done) => {
+      setup.freshInstance().then(api => {
+        apps = api.apps;
+        done();
+      });
+    });
+
+    appsTests(
+      () => apps.create,
+      () => apps.find,
+      () => apps.get
+    );
   });
 });
--- test/api/ProofsApi.spec.js
+++ test/api/ProofsApi.spec.js
@@ -2,39 +2,68 @@ const expect = require('expect.js');
 const setup = require('../helpers/setup');
 const TopazApi = require('../../src/topaz');
 
-describe('ProofsApi', function() {
-  let proofsApi;
-  let appId;
-
-  beforeEach('get a fresh api instance with an app', function (done) {
-    setup.freshInstanceLegacy().then(api => {
-      const appsApi = new TopazApi.AppsApi(api);
-      return Promise.all([appsApi.createApp({ name: 'test', interval: 3600 }), api]);
-    }).then(([{ data }, api]) => {
-      appId = data.id;
-      proofsApi = new TopazApi.ProofsApi(api);
-      done();
+describe('ProofsApi', () => {
+  const proofsTests = (appId, find) => {
+    describe('findProofs', () => {
+      const expects = (data, response, done) => {
+        expect(data).to.have.length(0);
+        expect(response.statusCode).to.be(200);
+        done();
+      };
+  
+      it('should call findProofs successfully using promises', (done) => {
+        find()(appId())
+        .then(({ data, response }) => {
+          expects(data, response, done)
+        });
+      });
+  
+      it('should call findProofs successfully using callbacks', (done) => {
+        find()(appId(), (_, data, response) => {
+          expects(data, response, done)
+        });
+      });
     });
+  };
+
+  describe('legacy interface', () => {
+    let proofsApi;
+    let appId;
+
+    beforeEach('get a fresh api instance with an app', (done) => {
+      setup.freshInstanceLegacy().then(api => {
+        const appsApi = new TopazApi.AppsApi(api);
+        return Promise.all([appsApi.createApp({ name: 'test', interval: 3600 }), api]);
+      }).then(([{ data }, api]) => {
+        appId = data.id;
+        proofsApi = new TopazApi.ProofsApi(api);
+        done();
+      });
+    });
+
+    proofsTests(
+      () => appId,
+      () => proofsApi.findProofs
+    );
   });
 
-  describe('findProofs', function () {
-    const expects = (data, response, done) => {
-      expect(data).to.have.length(0);
-      expect(response.statusCode).to.be(200);
-      done();
-    };
+  describe('new interface', () => {
+    let proofs;
+    let appId;
 
-    it('should call findProofs successfully using promises', function(done) {
-      proofsApi.findProofs(appId)
-      .then(({ data, response }) => {
-        expects(data, response, done)
+    beforeEach('get a fresh api instance with an app', (done) => {
+      setup.freshInstance().then(api => {
+        return Promise.all([api.apps.create({ name: 'test', interval: 3600 }), api]);
+      }).then(([{ data }, api]) => {
+        appId = data.id;
+        proofs = api.proofs;
+        done();
       });
     });
 
-    it('should call findProofs successfully using callbacks', function(done) {
-      proofsApi.findProofs(appId, (_, data, response) => {
-        expects(data, response, done)
-      });
-    });
+    proofsTests(
+      () => appId,
+      () => proofs.find
+    );
   });
 });
