184f4416a77ccded481ac5caafbb61505e173e75
Adam Gall <adam@decentcrypto.com>
2019-04-17 21:43:01 -0400

    Do more single-line thens

--- src/delightful/trust.js
+++ src/delightful/trust.js
@@ -1,14 +1,11 @@
 const multihash = require('../utils/hashing');
 
 const trust = (input, objectId, api) => {
-  return getObject(objectId, api).then(objectId => {
-    const hash = multihash(input);
-    return Promise.all([objectId, api.hashes.create(objectId, { hash })]);
-  }).then(([objectId, _]) => {
-    return api.objects.get(objectId);
-  }).then(({ data }) => {
-    return { data: input, object: data }
-  });
+  const hash = multihash(input);
+  return getObject(objectId, api)
+    .then(objectId => Promise.all([objectId, api.hashes.create(objectId, { hash })]))
+    .then(([objectId, _]) => api.objects.get(objectId))
+    .then(({ data }) => ({ data: input, object: data }));
 };
 
 const getObject = (objectId, api) => {
