4ca0988eab43f13d4f20f2c58bb7450cf06da7ae
Adam Gall <adam@decentcrypto.com>
2019-04-17 21:17:09 -0400

    Separate out the topaz base apis into a new function, and then use that as input to `trust`

--- src/delightful/trust.js
+++ src/delightful/trust.js
@@ -1,21 +1,21 @@
 const multihash = require('../utils/hashing');
 
-const trust = (input, objectId, objectsApi, hashesApi) => {
-  return getObject(objectId, objectsApi).then(objectId => {
+const trust = (input, objectId, api) => {
+  return getObject(objectId, api).then(objectId => {
     const hash = multihash(input);
-    return Promise.all([objectId, hashesApi.createHash(objectId, { hash })]);
+    return Promise.all([objectId, api.hashes.create(objectId, { hash })]);
   }).then(([objectId, _]) => {
-    return objectsApi.getObject(objectId);
+    return api.objects.get(objectId);
   }).then(({ data }) => {
     return { data: input, object: data }
   });
 };
 
-const getObject = (objectId, objectsApi) => {
+const getObject = (objectId, api) => {
   if (objectId !== null && objectId !== undefined) {
     return Promise.resolve(objectId);
   } else {
-    return objectsApi.createObject().then(({ data }) => data.id);
+    return api.objects.create().then(({ data }) => data.id);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ const Topaz = require('./topaz');
 const trust = require('./delightful/trust');
 
 const topazExports = (opts) => {
-  const api = new Topaz.ApiClient(opts);
-
-  const objectsApi = new Topaz.ObjectsApi(api);
-  const hashesApi = new Topaz.HashesApi(api);
-  const proofsApi = new Topaz.ProofsApi(api);
+  const api = baseApi(opts);
 
   return {
     appId: opts.appId,
+    trust: (input, objectId) => trust(input, objectId, api),
+    ...api
+  };
+};
 
-    trust: (input, objectId) => trust(input, objectId, objectsApi, hashesApi),
+const baseApi = (opts) => {
+  const apiClient = new Topaz.ApiClient(opts);
+  const objectsApi = new Topaz.ObjectsApi(apiClient);
+  const hashesApi = new Topaz.HashesApi(apiClient);
+  const proofsApi = new Topaz.ProofsApi(apiClient);
 
+  return {
     objects: {
       create: objectsApi.createObject,
       find: objectsApi.findObjects,
@@ -28,6 +33,6 @@ const topazExports = (opts) => {
       get: proofsApi.getProof
     }
   };
-}
+};
 
 module.exports = topazExports;
