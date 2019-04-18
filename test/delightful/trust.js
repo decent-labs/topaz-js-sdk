b976b9a0adc3891536c9f62d25d7142f0a857763
Adam Gall <adam@decentcrypto.com>
2019-04-17 20:33:48 -0400

    Add tests for the case when calling `trust` with an existing objectId

--- test/delightful/trust.js
+++ test/delightful/trust.js
@@ -25,4 +25,45 @@ describe('Trust', () => {
       });
     });
   });
+
+  describe('with an existing object', () => {
+    let objectId;
+
+    beforeEach('make an object', (done) => {
+      topaz.objects.create().then(({ data }) => {
+        objectId = data.id
+        done();
+      });
+    });
+
+    it('takes binarylike data and returns the data and existing api object', (done) => {
+      const inputData = 'hello world';
+      
+      topaz.trust(inputData, objectId).then(response => {
+        expect(response.data).to.be(inputData);
+        expect(response.object).to.be.an(Object);
+        expect(response.object.id).to.be(objectId);
+        expect(response.object.appId).to.be(topaz.appId);
+        expect(response.object.hashes[0].hash).to.be(multihash(inputData));
+        done();
+      });
+    });
+
+    it('appends a hash to the object hash list', (done) => {
+      const inputData = 'hello world';
+      const otherInputData = 'hello galaxy';
+
+      topaz.trust(inputData, objectId)
+      .then(_ => topaz.trust(otherInputData, objectId))
+      .then(response => {
+        expect(response.data).to.be(otherInputData);
+        expect(response.object.id).to.be(objectId);
+        expect(response.object.appId).to.be(topaz.appId);
+        expect(response.object.hashes).to.have.length(2);
+        expect(response.object.hashes[0].hash).to.be(multihash(inputData));
+        expect(response.object.hashes[1].hash).to.be(multihash(otherInputData));
+        done();
+      });
+    });
+  });
 });
