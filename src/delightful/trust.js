7010f856807489237dd833ca7e0e2df477a66bc8
Adam Gall <adam@decentcrypto.com>
2019-04-17 20:39:21 -0400

    Update the `trust` implementation to allow for either a null/undefined, or not, `objectId` to be passed

--- src/delightful/trust.js
+++ src/delightful/trust.js
@@ -1,14 +1,22 @@
 const multihash = require('../utils/hashing');
 
-const trust = (input, objectsApi, hashesApi) => {
-  const hash = multihash(input);
-  return objectsApi.createObject().then(({ data }) => {
-    return Promise.all([data, hashesApi.createHash(data.id, { hash })]);
-  }).then(([object, _]) => {
-    return objectsApi.getObject(object.id);
+const trust = (input, objectId, objectsApi, hashesApi) => {
+  return getObject(objectId, objectsApi).then(objectId => {
+    const hash = multihash(input);
+    return Promise.all([objectId, hashesApi.createHash(objectId, { hash })]);
+  }).then(([objectId, _]) => {
+    return objectsApi.getObject(objectId);
   }).then(({ data }) => {
     return { data: input, object: data }
   });
+};
+
+const getObject = (objectId, objectsApi) => {
+  if (objectId !== null && objectId !== undefined) {
+    return Promise.resolve(objectId);
+  } else {
+    return objectsApi.createObject().then(({ data }) => data.id);
+  }
 };
 
 module.exports = trust;
