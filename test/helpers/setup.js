647b32706f50aa461b959d8a335a27e318867487
Adam Gall <adam@decentcrypto.com>
2019-04-19 12:51:46 -0400

    Allow test setup method to be passed an app interval number

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -5,7 +5,7 @@ const TopazApi = require('../../src/topaz');
 const topaz = require('../../src/index');
 const random = require('./random');
 
-const apiSetup = () => {
+const apiSetup = (appInterval = 3600) => {
   const email = random.hexString(6) + '@' + random.hexString(6) + '.com';
   const password = random.hexString(20);
 
@@ -13,20 +13,20 @@ const apiSetup = () => {
     axios.post(process.env.BASE_PATH + '/users', { email, password })
       .then(_ => axios.post(process.env.BASE_PATH + '/auth/login', { email, password }))
       .then(res => axios.post(process.env.BASE_PATH + '/tokens', {}, { headers: { 'Authorization': res.data.token }}))
-      .then(res => Promise.all([res.data.token, axios.post(process.env.BASE_PATH + '/apps', { name: 'test app', interval: 3600 }, { headers: { 'Authorization': res.data.token }})]))
+      .then(res => Promise.all([res.data.token, axios.post(process.env.BASE_PATH + '/apps', { name: 'test app', interval: appInterval }, { headers: { 'Authorization': res.data.token }})]))
       .then(([ apiKey, res ]) => resolve({ apiKey, appId: res.data.id }))
       .catch(err => reject(err));
   });
 }
 
-const freshInstanceLegacy = () => {
-  return apiSetup().then(({ apiKey, appId }) => {
+const freshInstanceLegacy = (appInterval) => {
+  return apiSetup(appInterval).then(({ apiKey, appId }) => {
     return new TopazApi.ApiClient({ basePath: process.env.BASE_PATH, apiKey, appId })
   });
 }
 
-const freshInstance = () => {
-  return apiSetup().then(({ apiKey, appId }) => {
+const freshInstance = (appInterval) => {
+  return apiSetup(appInterval).then(({ apiKey, appId }) => {
     return topaz({ basePath: process.env.BASE_PATH, apiKey, appId });
   });
 }
